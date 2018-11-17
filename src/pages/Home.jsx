2ec0736e9dde9308175497baae2481696959239b
jrclegg <clegg.jonathan@rocketmail.com>
2018-11-17 19:15:07 +0000

    make homepage /

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,38 @@
 import React from 'react';
+import styled from 'styled-components'
+import Header from '../components/Header'
+import Button from '../components/Button/Button'
+import Facebook from '../components/Facebook';
 
-const Home = () => (
-    <div>
-       <h2>You are now logged in!!!</h2>
-    </div>
-  );
+const LeftDiv = styled.div`
+ float: left;
+`
+const RightDiv = styled.div`
+ margin-right: 30px;
+ float: right;
+`
+const FacebookStyle = {
+ backgroundColor: '#4267B2',
+};
 
-export default Home
-  
+const TwitterStyle = {
+ backgroundColor: '#1DA1F2',
+};
+
+const LinkedInStyle = {
+ backgroundColor: '#0077B5',
+};
+
+const Login = () => (
+        <div>
+         <Header>
+         </Header>
+         <LeftDiv>
+         </LeftDiv>
+        <RightDiv>
+         <Facebook/>
+        </RightDiv>
+        </div>
+)
+
+export default Login
