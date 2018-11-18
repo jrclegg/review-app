3079bf031a7bdd983573da07a4d4ce645298a7e2
jrclegg <clegg.jonathan@rocketmail.com>
2018-11-18 10:58:08 +0000

    Make Home component a class component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,38 +1,26 @@
-import React from 'react';
-import styled from 'styled-components'
-import Header from '../components/Header'
-import Button from '../components/Button/Button'
+import React, {Component} from 'react';
+
+import Header from '../components/Header';
 import Facebook from '../components/Facebook';
+import LoginForm from '../components/Form';
+import FormWrapper from '../components/FormWrapper'
 
-const LeftDiv = styled.div`
- float: left;
-`
-const RightDiv = styled.div`
- margin-right: 30px;
- float: right;
-`
-const FacebookStyle = {
- backgroundColor: '#4267B2',
-};
+class Home extends Component {
+	constructor(props){
+		super(props);
+	}
+	render(){
+		return(
+		<div>
+			<Header/>
+			<FormWrapper>
+				<Facebook/>
+				<LoginForm/>
+			</FormWrapper>
+		</div>
+		)
+	}
 
-const TwitterStyle = {
- backgroundColor: '#1DA1F2',
-};
+}
 
-const LinkedInStyle = {
- backgroundColor: '#0077B5',
-};
-
-const Login = () => (
-        <div>
-         <Header>
-         </Header>
-         <LeftDiv>
-         </LeftDiv>
-        <RightDiv>
-         <Facebook/>
-        </RightDiv>
-        </div>
-)
-
-export default Login
+export default Home
